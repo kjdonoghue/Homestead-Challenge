ba8173dce66844b3c606be6f8e30f2decdce40a7
Katy Donoghue <kathrynjdonoghue@gmail.com>
2021-02-10 16:35:05 -0500

    started building add veg

--- client/src/components/AddVegetable.js
+++ client/src/components/AddVegetable.js
@@ -1,11 +1,30 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: 200,
+        },
+    },
+}));
 
 function AddVegetable() {
+    const classes = useStyles();
 
-
-    return(
+    return (
         <div>
-      
+            <form className={classes.root} noValidate autoComplete="off">                
+                <TextField id="outlined-basic" label="Variety" variant="outlined" />
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            </form>
+
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import BaseLayout from './components/BaseLayout'
 import VegetableTracker from './components/VegetableTracker'
+import AddVegetable from './components/AddVegetable'
 import PerennialTracker from './components/PerrenialTracker'
+import AddPerrenial from './components/AddPerrenial'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 
@@ -15,7 +17,9 @@ ReactDOM.render(
         <BaseLayout>
           <Route component={App} path='/' exact />
           <Route component={VegetableTracker} path='/vegetables' />
+          <Route component={AddVegetable} path='/add-vegetable' />
           <Route component={PerennialTracker} path='/perennials' />
+          <Route component={AddPerrenial} path='/add-perennials' />
         </BaseLayout>
       </Switch>
     </BrowserRouter>
